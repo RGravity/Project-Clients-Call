7b400a3898b24ea0eded1fc0ecbfd5ecf859a059
KingdomHearts <michaelbos@live.nl>
2015-09-01 11:58:06 +0200

    Standard Prototype Scene And one for Rogier

---
+++ Project/Project-Clients-Call/Assets/Scripts/RaceScipt.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceScipt : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        GameObject.FindObjectOfType<RaceScipt>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetAxis("Horizontal") == -1)
+        {
+
+        }
+	}
+}
