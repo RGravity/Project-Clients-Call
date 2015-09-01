47e74b5fd4d2c624a7c6b14742d5a75b182404a9
RGravity <rogieroh@hotmail.com>
2015-09-01 13:50:45 +0200

    Basic Controls

---
+++ Project/Project-Clients-Call/Assets/Scripts/LevelScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelScript : MonoBehaviour {
+
+    private float _speed = 10f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            _speed += 0.01f;
+            gameObject.transform.position = transform.position - (transform.forward * _speed * Time.deltaTime);
+        }
+	}
+}
--- Project/Project-Clients-Call/Assets/Scripts/RaceScipt.cs
+++ Project/Project-Clients-Call/Assets/Scripts/RaceScipt.cs
@@ -4,16 +4,15 @@ using System.Collections;
 public class RaceScipt : MonoBehaviour {
 
 
+
 	// Use this for initialization
 	void Start () {
-        GameObject.FindObjectOfType<RaceScipt>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetAxis("Horizontal") == -1)
-        {
-
-        }
+        Vector3 moveDir = Vector3.zero;
+        moveDir.x = Input.GetAxis("Horizontal") * 4; // get result of AD keys in X
+        transform.position += moveDir * Time.deltaTime;
 	}
 }
