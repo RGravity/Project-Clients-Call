b66ef609342cfea48d9aec329d1e56fb1df82d4d
Elias1355 <eliasdragon55@hotmail.com>
2015-09-03 13:05:01 +0200

    I'm pregnant
    Im the mommy

---
+++ Project/Project-Clients-Call/Assets/Scripts/BackButtonScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButtonScript : MonoBehaviour {
+
+    // Use this for initialization
+    private GameObject _camera;
+    float timer = 0;
+    private bool _rotation = false;
+    float degrees = 270;
+
+	void Start () 
+    {
+        _camera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+        if (_rotation == true)
+        {
+            Debug.Log(degrees);
+            degrees = degrees + 1;
+            _camera.transform.rotation = Quaternion.Euler(degrees, 0, 0);
+
+
+            if (degrees == 360 )
+            {
+                _rotation = false;
+                _camera.transform.rotation = Quaternion.Euler(0, 0, 0);
+                degrees = 270;
+            }
+
+        }
+	
+	}
+
+    void OnMouseDown()
+    {
+        _rotation = true;
+
+    }
+}
---
+++ Project/Project-Clients-Call/Assets/Scripts/OptionsCameraRotation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	// Use this for initialization
+    private GameObject _camera;
+    float timer = 0;
+    private bool _rotation = false;
+    float degrees = 0;
+	void Start () 
+    {
+	 _camera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (_rotation == true)
+        {
+           
+                degrees = degrees - 1;
+                _camera.transform.rotation = Quaternion.Euler (degrees, 0, 0);
+
+
+            if (degrees <=-90)
+            {
+                _rotation = false;
+                _camera.transform.rotation = Quaternion.Euler(270, 0, 0);
+                degrees = 0;
+            }
+   
+        }
+     
+	}
+
+    void OnMouseDown()
+    {
+       _rotation = true;
+        
+    }
+}
