03e8a579a21529fd36952721721421db90b4c426
RGravity <rogieroh@hotmail.com>
2015-09-04 15:54:38 +0200

    Banking

---
+++ Project/Project-Clients-Call/Assets/Scripts/BankingP2Script.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BankingP2Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveRot = Vector3.zero;
+        moveRot.y = -(Input.GetAxis("Horizontal P2") * 12);
+        gameObject.transform.localEulerAngles = moveRot * 3;
+	}
+}
