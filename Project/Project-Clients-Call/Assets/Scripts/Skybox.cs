66d21c392674e8362caaed4a74ece59a4514f922
RGravity <rogieroh@hotmail.com>
2015-09-08 16:43:46 +0200

    FUN

---
+++ Project/Project-Clients-Call/Assets/Scripts/Skybox.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skybox : MonoBehaviour {
+    
+    public Texture[] textures;
+    public Renderer rend;
+    int index = 0;
+    Vector3 rotation;
+    Vector3 currentRotation;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        //textures = Resources.Load("Background") as Texture[];
+    }
+
+    void Update()
+    {
+        if (textures.Length == 0)
+            return;
+
+        index++;
+        if (index >= textures.Length)
+        {
+            index = 0;
+        }
+        rend.material.mainTexture = textures[index];
+        currentRotation = transform.localEulerAngles;
+        rotation = currentRotation + new Vector3(0, 0.10f, 0);
+        transform.localEulerAngles = rotation; 
+
+    }
+}
