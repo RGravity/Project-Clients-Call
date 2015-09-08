fd53cfd366f0bf130019f6fa3e71c28f10621498
RGravity <rogieroh@hotmail.com>
2015-09-08 14:24:43 +0200

    Changed Stuffz

--- Project/Project-Clients-Call/Assets/Scripts/ShootScript.cs
+++ Project/Project-Clients-Call/Assets/Scripts/ShootScript.cs
@@ -12,13 +12,13 @@ public class ShootScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.M) && this.name == "Player 1")
+        if (Input.GetButtonDown("Fire2P1") && this.name == "Player 1")
         {
             bulletPrefab.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 1.3f);
             _bullet = Instantiate(bulletPrefab).GetComponent<Transform>();
             _bullet.GetComponent<Bullet>().Player(true);
         }
-        if (Input.GetKeyDown(KeyCode.F) && this.name == "Player 2")
+        if (Input.GetButtonDown("Fire2P2") && this.name == "Player 2")
         {
             bulletPrefab.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 1.3f);
             _bullet = Instantiate(bulletPrefab).GetComponent<Transform>();
