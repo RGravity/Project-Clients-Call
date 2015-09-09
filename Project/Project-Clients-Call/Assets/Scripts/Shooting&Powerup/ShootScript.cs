7806d9588230afff2db8d35207e4a4aa6bc7f2f4
RGravity <rogieroh@hotmail.com>
2015-09-09 16:13:16 +0200

    Organized shit

--- Project/Project-Clients-Call/Assets/Scripts/Shooting&Powerup/ShootScript.cs
+++ Project/Project-Clients-Call/Assets/Scripts/Shooting&Powerup/ShootScript.cs
@@ -5,21 +5,27 @@ public class ShootScript : MonoBehaviour {
 
     public GameObject bulletPrefab;
     private Transform _bullet;
+    private float timeStampP1;
+    private float timeStampP2;
+    private int coolDownPeriodInSeconds = 3;
 	// Use this for initialization
 	void Start () {
-	
+        timeStampP1 = Time.time;
+        timeStampP2 = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Fire2P1") && this.name == "Player 1")
+        if (Input.GetButtonDown("Fire2P1") && this.name == "Player 1" && timeStampP1 <= Time.time)
         {
+            timeStampP1 = Time.time + coolDownPeriodInSeconds;
             bulletPrefab.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 1.3f);
             _bullet = Instantiate(bulletPrefab).GetComponent<Transform>();
             _bullet.GetComponent<Bullet>().Player(true);
         }
-        if (Input.GetButtonDown("Fire2P2") && this.name == "Player 2")
+        if (Input.GetButtonDown("Fire2P2") && this.name == "Player 2" && timeStampP2 <= Time.time)
         {
+            timeStampP2 = Time.time + coolDownPeriodInSeconds;
             bulletPrefab.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 1.3f);
             _bullet = Instantiate(bulletPrefab).GetComponent<Transform>();
             _bullet.GetComponent<Bullet>().Player(false);
