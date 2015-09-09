40c45ca5fe944aa34cf02922013b368891fd278a
Elias1355 <eliasdragon55@hotmail.com>
2015-09-09 12:11:59 +0200

    something to commit

---
+++ Project/Project-Clients-Call/Assets/Scripts/TutorialScript/TutorialScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialScript : MonoBehaviour {
+
+    private GameObject _tutorialTextOne;
+    private GameObject _tutorialTextTwo;
+    private GameObject _tutorialTextThree;
+    private GameObject _tutorialTextFour;
+    private GameObject _tutorialTextFive;
+
+	// Use this for initialization
+	void Start () 
+    {
+        _tutorialTextOne = GameObject.FindGameObjectWithTag("TutorialOne");
+
+	}
+	
+	// Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _tutorialTextOne.GetComponent<Rigidbody> ())
+        {
+            Debug.Log(true);
+        }
+    }
+}
