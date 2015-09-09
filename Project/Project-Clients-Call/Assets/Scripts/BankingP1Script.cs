d61fb330651f9eaa99967436caddae622be69e73
KingdomHearts <michaelbos@live.nl>
2015-09-09 18:02:52 +0200

    `Last commit of the day

--- Project/Project-Clients-Call/Assets/Scripts/BankingP1Script.cs
+++ Project/Project-Clients-Call/Assets/Scripts/BankingP1Script.cs
@@ -5,6 +5,7 @@ public class BankingP1Script : MonoBehaviour {
 
     Vector3 CameraPosition;
     Vector3 PlayerPosition;
+    Vector3 moveRot = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +15,21 @@ public class BankingP1Script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 moveRot = Vector3.zero;
+        GameObject.Find("P1 Camera").GetComponent<Camera>().transform.localEulerAngles = CameraPosition + moveRot;
         moveRot.y = -(Input.GetAxis("Horizontal P1") * 12);
         gameObject.transform.localEulerAngles = PlayerPosition + moveRot * 3;
 
-        GameObject.Find("P1 Camera").GetComponent<Camera>().transform.localEulerAngles = CameraPosition + moveRot;
+        Debug.Log(gameObject.transform.localEulerAngles);
+        //if (Input.GetAxis("Horizontal P1") > 0)
+        //{
+        //   gameObject.transform.localEulerAngles = gameObject.transform.localEulerAngles + new Vector3(gameObject.transform.localEulerAngles.x, gameObject.transform.localEulerAngles.y + 1, gameObject.transform.localEulerAngles.z);
+        //}
+        //if (Input.GetAxis("Horizontal P1") < 0)
+        //{
+        //    gameObject.transform.localEulerAngles = gameObject.transform.localEulerAngles + new Vector3(gameObject.transform.localEulerAngles.x, gameObject.transform.localEulerAngles.y - 1, gameObject.transform.localEulerAngles.z);
+        //}
+
+
         //Sync stuff
 	}
 }
