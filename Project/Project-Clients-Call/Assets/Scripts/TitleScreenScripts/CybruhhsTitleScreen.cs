62942b1edbd39f51ce71fe54b5c247046aa57982
KingdomHearts <michaelbos@live.nl>
2015-09-14 21:29:39 +0200

    Allemaal plaatjes.

---
+++ Project/Project-Clients-Call/Assets/Scripts/TitleScreenScripts/CybruhhsTitleScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CybruhhsTitleScreen : MonoBehaviour {
+
+	// Use this for initialization
+    private float screenTime = 0;
+    // Use this for initialization
+    void Start()
+    {
+        screenTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > (screenTime + 2.5f))
+        {
+            Application.LoadLevel(2);
+        }
+    }
+}
