4764f673c88ea55873c0c2b6c78f57231ef88dab
KingdomHearts <michaelbos@live.nl>
2015-09-16 21:06:11 +0200

    Bullet

--- Project/Project-Clients-Call/Assets/Scripts/Shooting&Powerup/Bullet.cs
+++ Project/Project-Clients-Call/Assets/Scripts/Shooting&Powerup/Bullet.cs
@@ -11,8 +11,13 @@ public class Bullet : MonoBehaviour {
     private Camera _playerCameraP1;
     private Camera _playerCameraP2;
     private float distance = 30.0f;
-    private List<float> spawnPoints = new List<float>(){-4,-2,0,2,4};
+    //private List<float> spawnPoints = new List<float>(){-4,-2,0,2,4};
+    private List<float> spawnPoints = new List<float>(){7.4f,5.4f,3.4f,1.4f,-0.6f,-2.6f,-4.6f};
     private GameObject _pointBlock;
+    private float shootingWallTimer = 0;
+    private float shootingWallCount = 0;
+    private GameObject otherCube;
+    private GameObject hitted;
 
 	// Use this for initialization
 	void Start () {
@@ -30,47 +35,90 @@ public class Bullet : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         _body.velocity = new Vector3(0, 0, 40);
-
-	}
+            if (Time.time > (shootingWallTimer + 1f))
+            {
+                shootingWallTimer = Time.time;
+                shootingWallCount++;
+            }
+            if (hitted.gameObject.GetComponent<MeshRenderer>().enabled == true)
+            {
+                hitted.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+            else if (hitted.gameObject.GetComponent<MeshRenderer>().enabled == false)
+            {
+                hitted.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
+            else
+            {
+                shootingWallCount = 0;
+            }
+            if (Time.time > (shootingWallTimer + 1f))
+            {
+                shootingWallTimer = Time.time;
+                shootingWallCount++;
+            }
+            if (otherCube.gameObject.GetComponent<MeshRenderer>().enabled == true)
+            {
+                otherCube.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+            else if (otherCube.gameObject.GetComponent<MeshRenderer>().enabled == false)
+            {
+                otherCube.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
+            else
+            {
+                shootingWallCount = 0;
+            }
+        }
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name.Contains("Cube") || other.gameObject.name.Contains("TrackWall"))
+        if (other.gameObject.name.Contains("Cube") || other.gameObject.name.Contains("Wall"))
         {
             if (_isPlayer1)
             {
-                GameObject otherCube = Instantiate(other.gameObject);
-                //Vector3 oldScale = other.gameObject.transform.localScale;
-                //Vector3 hitScale = other.GetComponent<BoxCollider>().size;
-                //otherCube.GetComponent<BoxCollider>().size = hitScale;
-                //otherCube.gameObject.transform.localScale = oldScale;
+                hitted = other.gameObject;
                 Vector3 position;
                 Collider[] hit;
                 do
                 {
                     //position = _playerCameraP2.transform.position + _playerCameraP2.transform.forward + new Vector3(2, 1, 30);
-                    position = new Vector3(_pointBlock.transform.position.x + spawnPoints[Random.Range(0,spawnPoints.Count)], _planeP2.transform.position.y-1, _playerCameraP2.transform.forward.z + 30);
+                    position = new Vector3(spawnPoints[Random.Range(0,spawnPoints.Count)], other.gameObject.transform.localPosition.y - 3.9f, other.gameObject.transform.localPosition.z);
+                    otherCube = (GameObject)Instantiate(other.gameObject, position, Quaternion.identity);
                     hit = Physics.OverlapSphere(position, otherCube.transform.localScale.x * 2);
                 }
                 while (hit.Length == 1);
+
+                //otherCube.gameObject.transform.localPosition = other.gameObject.transform.localPosition;
+                //otherCube.gameObject.transform.localPosition = new Vector3(other.gameObject.transform.localPosition.x, other.gameObject.transform.localPosition.y - 6, other.gameObject.transform.localPosition.z);
+                //otherCube.transform.localEulerAngles = new Vector3(0, 90, 0);
+                Vector3 oldScale = other.gameObject.transform.localScale;
+                Vector3 hitScale = other.GetComponent<BoxCollider>().size;
+
                 //otherCube.transform.localScale = new Vector3(2, 1, 1);
                 otherCube.transform.position = position;
                 otherCube.transform.rotation = new Quaternion(0.0f, _playerCameraP2.transform.rotation.y, 0.0f, _playerCameraP2.transform.rotation.w);
                 //otherCube.transform.position = new Vector3(other.transform.position.x, -other.transform.position.y, other.transform.position.z);
                 //otherCube.transform.rotation = new Quaternion(other.transform.rotation.x, 180, other.transform.rotation.z, 1);
                 //otherCube.transform.position = new Vector3(100, 100, 100);
-                otherCube.transform.parent = _planeP2.transform;
+                otherCube.transform.parent = GameObject.Find("TrackBlocks2").gameObject.transform;
+                otherCube.transform.localEulerAngles = new Vector3(0, 90, 0);
+
+               
             }
             else if (_isPlayer1 == false)
             {
                 GameObject otherCube = Instantiate(other.gameObject);
+                otherCube.gameObject.transform.localPosition = other.gameObject.transform.localPosition;
+                other.gameObject.transform.localPosition = new Vector3(other.gameObject.transform.localPosition.x, other.gameObject.transform.localPosition.y + 6, other.gameObject.transform.localPosition.z);
                 Vector3 oldScale = other.gameObject.transform.localScale;
                 Vector3 hitScale = other.GetComponent<BoxCollider>().size;
+
                 Vector3 position;
                 Collider[] hit;
                 do
                 {
                     //position = _playerCameraP1.transform.position + _playerCameraP1.transform.forward + new Vector3(0, -1, 30);
-                    position = new Vector3(_pointBlock.transform.position.x + spawnPoints[Random.Range(0, spawnPoints.Count)], _planeP2.transform.position.y+0.6f, _playerCameraP2.transform.forward.z + 30);
+                    position = new Vector3(spawnPoints[Random.Range(0, spawnPoints.Count)], other.gameObject.transform.localPosition.y + 6, other.gameObject.transform.localPosition.z);
                     hit = Physics.OverlapSphere(position, otherCube.transform.localScale.x * 2);
                 }
                 while (hit.Length == 1);
@@ -78,7 +126,8 @@ public class Bullet : MonoBehaviour {
                 otherCube.transform.position = position;
                 otherCube.transform.rotation = new Quaternion(0.0f, _playerCameraP1.transform.rotation.y, 0.0f, _playerCameraP1.transform.rotation.w);
                 //otherCube.transform.position = new Vector3(other.transform.position.x, -other.transform.position.y, other.transform.position.z);
-                otherCube.transform.parent = _planeP1.transform;
+                otherCube.transform.parent = GameObject.Find("TrackBlocks12").gameObject.transform;
+                otherCube.transform.localEulerAngles = new Vector3(0, 90, 0);
             }
             //GameObject otherCube = Instantiate(other.gameObject);
             //otherCube.transform.position = new Vector3(other.transform.position.x, -other.transform.position.y, other.transform.position.z);
