c6e179b04fe254bfeb8f49f55c13e6f583675f16
RGravity <rogieroh@hotmail.com>
2015-09-16 20:33:35 +0200

    hey

--- Project/Project-Clients-Call/Assets/Scripts/MenuScript/Pause/PauseScript.cs
+++ Project/Project-Clients-Call/Assets/Scripts/MenuScript/Pause/PauseScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PauseScript : MonoBehaviour {
 
+	private bool pauseGame = false;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,6 +17,21 @@ public class PauseScript : MonoBehaviour {
 
     void CheckPause()
     {
-        
+		if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button7) || Input.GetKeyDown(KeyCode.Joystick2Button7))
+		{
+			pauseGame = !pauseGame;
+			
+			if (pauseGame == true)
+			{
+				Time.timeScale = 0;
+				pauseGame = true;
+			}
+		}
+		
+		if (pauseGame == false)
+		{
+			Time.timeScale = 1;
+            pauseGame = false;
+		}
     }
 }
