ad28993a802e033925e494156aebe681951951ed
Elias1355 <eliasdragon55@hotmail.com>
2015-09-17 13:43:05 +0200

    commiting

--- Project/Project-Clients-Call/Assets/Scripts/MenuScript/Pause/PauseScript.cs
+++ Project/Project-Clients-Call/Assets/Scripts/MenuScript/Pause/PauseScript.cs
@@ -4,7 +4,8 @@ using System.Collections;
 
 public class PauseScript : MonoBehaviour {
 
-	private bool pauseGame = false;
+	public bool pauseGame = false;
+    private bool _moving = false;
 
 	// Use this for initialization
 	void Start () {
@@ -40,5 +41,46 @@ public class PauseScript : MonoBehaviour {
 			Time.timeScale = 1;
             pauseGame = false;
 		}
+        Selection();
+    }
+
+    void Selection ()
+    {
+        if (pauseGame && Input.GetAxis("Vertical P1") < -0.5f || pauseGame && Input.GetAxis("Vertical P2") < -0.5f)
+        {
+            GameObject.Find("Canvas").GetComponent<Image>().enabled = true;
+            GameObject.Find("ResumeArrows").GetComponent<Image>().enabled = false;
+            GameObject.Find("QuitArrows").GetComponent<Image>().enabled = true;
+        
+        }
+
+        if (pauseGame && Input.GetAxis("Vertical P1") > 0.5f || pauseGame && Input.GetAxis("Vertical P2") > 0.5f)
+        {
+            GameObject.Find("Canvas").GetComponent<Image>().enabled = true;
+            GameObject.Find("ResumeArrows").GetComponent<Image>().enabled = true;
+            GameObject.Find("QuitArrows").GetComponent<Image>().enabled = false;
+        }
+
+    }
+
+    void Choice()
+    {
+        if (pauseGame && GameObject.Find("ResumeArrows").GetComponent<Image>() == enabled && Input.GetButton ("FireP1") ||
+            pauseGame && GameObject.Find("ResumeArrows").GetComponent<Image>() == enabled && Input.GetButton("FireP2"))
+        {
+            pauseGame = false;
+
+        }
+
+        if (pauseGame && GameObject.Find("ResumeArrows").GetComponent<Image>() == enabled && Input.GetButton("FireP1") ||
+            pauseGame && GameObject.Find("ResumeArrows").GetComponent<Image>() == enabled && Input.GetButton("FireP2"))
+        {
+            Application.LoadLevel(2);
+            GameObject.FindObjectOfType<MusicScript>().Play2 = true;
+            GameObject.FindObjectOfType<ConfirmScript>().bodyPlayer1 = 0;
+            GameObject.FindObjectOfType<ConfirmScript>().bodyPlayer2 = 0;
+            GameObject.FindObjectOfType<ConfirmScript>().SavedP1Score = 0;
+            GameObject.FindObjectOfType<ConfirmScript>().SavedP2Score = 0;
+        }
     }
 }
