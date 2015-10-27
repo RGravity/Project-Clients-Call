baef39ec81f956b703d98d98a32a3e8220b4e86b
RGravity <rogieroh@hotmail.com>
2015-10-27 10:19:09 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Project/Project-Clients-Call/Assets/Prefabs/Bullet.prefab
    	Project/Project-Clients-Call/Assets/Scene/AlphaScene MMRMichael.unity

---
+++ Project/Project-Clients-Call/Assets/Scripts/Shooting&Powerup/PowerupPickup.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupPickup : MonoBehaviour {
+
+    Vector3 rotation;
+    Vector3 currentRotation;
+    private int _zBlocks;
+    public int ZBlocks { set { _zBlocks = value; } }
+    private bool respawn = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //currentRotation = transform.localEulerAngles;
+        //rotation = currentRotation + new Vector3(0, 1f, 0);
+        //transform.localEulerAngles = rotation; 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == GameObject.FindObjectOfType<PowerUpScriptP1>().name)
+        {
+            if (!GameObject.FindObjectOfType<PowerUpScriptP1>().PowerUpAvailable)
+            {
+                GameObject.FindObjectOfType<PowerUpScriptP1>().PowerUp = PowerUpScriptP1.Powerup.Boost;
+                GameObject.FindObjectOfType<PowerUpScriptP1>().PowerUpAvailable = true;
+            }
+            GameObject.FindGameObjectWithTag("PowerUp").GetComponent<AudioSource>().Play();
+            respawn = true;
+            Destroy(gameObject);
+        }
+        if (other.gameObject.name == GameObject.FindObjectOfType<PowerUpScriptP2>().name)
+        {
+            if (!GameObject.FindObjectOfType<PowerUpScriptP2>().PowerUpAvailable)
+            {
+                GameObject.FindObjectOfType<PowerUpScriptP2>().PowerUp = PowerUpScriptP2.Powerup.Boost;
+                GameObject.FindObjectOfType<PowerUpScriptP2>().PowerUpAvailable = true;
+            }
+            GameObject.FindGameObjectWithTag("PowerUp").GetComponent<AudioSource>().Play();
+            respawn = true;
+            Destroy(gameObject);
+
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (respawn)
+        {
+            Vector3 newPosition = this.transform.position;
+            newPosition.z += _zBlocks;
+            GameObject.FindObjectOfType<TrackBuildScript>().SpawnPowerup(newPosition, this.gameObject.transform.parent.gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        respawn = true;
+        Destroy(this.gameObject);
+
+    }
+}
